8e25922c43d38c4aca03ec187e724caed9b35eb7
Weijie Wang <duangsir518@163.com>
2025-10-21 17:26:27 +0200

    Updates default SMPLX motion file path
    Changes the default path for the SMPLX motion file to a new location.
    
    This update ensures the script uses the correct motion data by default,
    preventing potential errors or unexpected behavior due to an outdated or
    incorrect file path.

--- scripts/smplx_to_robot.py
+++ scripts/smplx_to_robot.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
         help="SMPLX motion file to load.",
         type=str,
         # required=True,
-        default="/home/yanjieze/projects/g1_wbc/GMR/motion_data/ACCAD/Male1General_c3d/General_A1_-_Stand_stageii.npz",
+        default= "/home/wang/GMR/assets/body_models/ACCAD/Male2Running_c3d/C1_-_stand_to_run_stageii.npz",
+        # default="/home/yanjieze/projects/g1_wbc/GMR/motion_data/ACCAD/Male1General_c3d/General_A1_-_Stand_stageii.npz",
         # default="/home/yanjieze/projects/g1_wbc/GMR/motion_data/ACCAD/Male2MartialArtsKicks_c3d/G8_-__roundhouse_left_stageii.npz"
         # default="/home/yanjieze/projects/g1_wbc/TWIST-dev/motion_data/AMASS/KIT_572_dance_chacha11_stageii.npz"
         # default="/home/yanjieze/projects/g1_wbc/GMR/motion_data/ACCAD/Male2MartialArtsPunches_c3d/E1_-__Jab_left_stageii.npz",
