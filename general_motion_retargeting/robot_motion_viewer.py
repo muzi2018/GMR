1136e6bfd4c0d0c047d9bd80ca407a1992bc831e
Weijie Wang <duangsir518@163.com>
2025-10-22 17:57:55 +0200

    update IK configure

--- general_motion_retargeting/robot_motion_viewer.py
+++ general_motion_retargeting/robot_motion_viewer.py
@@ -207,23 +207,23 @@ class RobotMotionViewer:
                 
         for i in range(self.model.nbody):
             body_name = self.model.body(i).name
-            pos = self.data.xpos[i]           # global position of the link
-            rot = self.data.xmat[i].reshape(3, 3)  # global rotation matrix of the link , left_elbow, left_elbow_link
-            quat_xyzw = R.from_matrix(rot).as_quat()
-            quat_wxyz = np.roll(quat_xyzw, 1)
+            R_pos = self.data.xpos[i]           # global position of the link
+            R_rot = self.data.xmat[i].reshape(3, 3)  # global rotation matrix of the link , left_elbow, left_elbow_link
+            R_xyzw = R.from_matrix(R_rot).as_quat()
+            R_wxyz = np.roll(R_xyzw, 1)
             
             if human_motion_data is not None and body_name in self.ik_match_table1:
                 human_body_name = self.ik_match_table1[body_name][0]
-                (hum_pos, hum_rot) = human_motion_data[human_body_name]
-                rel_vec = pos - hum_pos # robot link pos - human link pos
-                rel_rot = R.from_matrix(rot).inv() * R.from_quat(hum_rot, scalar_first=True)
-                print(f"[blue]{human_body_name} , {hum_pos}, {body_name} {pos}, Rel Vector {rel_vec}")
-                print(f"[yellow]{human_body_name} , {hum_rot}, {body_name} {quat_wxyz}, Rel Vector {rel_rot.as_quat()}")
-                
+                (H_pos, H_rot) = human_motion_data[human_body_name]
+                rel_vec = R_pos - H_pos # robot link pos - human link pos
+                rel_rot = R.from_quat(H_rot, scalar_first=True).inv() * R.from_quat(R_wxyz, scalar_first=True)
+                print(f"[blue]{human_body_name} , {H_pos}, {body_name} {R_pos}, Rel Vec {rel_vec}")
+                print(f"[yellow]{human_body_name} , {H_rot}, {body_name} {R_wxyz}, Rel Angl_Vec {rel_rot.as_quat(scalar_first=True)}")
+
             
                 draw_frame(
-                    pos,
-                    rot,
+                    R_pos,
+                    R_rot,
                     self.viewer,
                     size=0.05,  # adjust size for visualization
                     joint_name=body_name  # optional, label the link
